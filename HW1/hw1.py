8c3f3856320b79988d995f655e6c1ff45ff0bad7
Yaniv Bronshtein <yanbronshtein@gmail.com>
2020-03-03 21:47:22 -0500

    Successfully lower-cased each word

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -2,29 +2,40 @@ import os
 import shutil
 
 # Get Working directory from user
-path = input("Please enter the file path to a chosen working directory containing files test.txt and train.txt\n"
+path = input("Please enter the file path to a chosen working directory containing files train.txt and test.txt\n"
              "This can be achieved by typing pwd in a linux shell if you are already in that directory\n")
 
 
 # Source paths of train.txt and test.txt
-sources = [path + "/test.txt", path + "/train.txt"]
+sources = [path + "/train.txt", path + "/test.txt"]
 
+# List of sentences in test.txt padded with <s> and </s>
+padded_train_data = []
+# List of sentences in test.txt padded with <s> and </s>
+padded_test_data = []
 
-clean_train_data = []
-clean_test_data = []
-
-# Extract data from test.txt
+# Extract data from train.txt and store in padded_train_data
 file0 = open(sources[0], 'r')
 while line := file0.readline():
-    new_line0 = "<s> " + str(line).replace("\n", " </s>")
-    clean_train_data.append(new_line0)
+    tokenized_line = line.split()
+    paddedLine = ""
+    paddedLine += "<s>"
+    for token in tokenized_line:
+        paddedLine += token.lower() + " "
+    paddedLine += "</s>"
+    padded_train_data.append(paddedLine)
 file0.close()
 
-# Extract data from train.txt
+# Extract data from test.txt and store in padded_test_data
 file1 = open(sources[1], 'r')
 while line := file1.readline():
-    new_line1 = "<s> " + str(line).replace("\n", " </s>\n")
-    clean_test_data.append(new_line1)
+    tokenized_line = line.split()
+    paddedLine = ""
+    paddedLine += "<s>"
+    for token in tokenized_line:
+        paddedLine += token.lower() + " "
+    paddedLine += "</s>"
+    padded_test_data.append(paddedLine)
 file1.close()
 
 
