50bc79540b3e3ffcaa02e9760734b083edd56681
Yaniv Bronshtein <yanbronshtein@gmail.com>
2020-03-03 23:58:32 -0500

    Succesfully created training dictionary

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -5,7 +5,6 @@ import shutil
 path = input("Please enter the file path to a chosen working directory containing files train.txt and test.txt\n"
              "This can be achieved by typing pwd in a linux shell if you are already in that directory\n")
 
-
 # Source paths of train.txt and test.txt
 sources = [path + "/train.txt", path + "/test.txt"]
 
@@ -19,7 +18,7 @@ file0 = open(sources[0], 'r')
 while line := file0.readline():
     tokenized_line = line.split()
     paddedLine = ""
-    paddedLine += "<s>"
+    paddedLine += "<s> "
     for token in tokenized_line:
         paddedLine += token.lower() + " "
     paddedLine += "</s>"
@@ -28,16 +27,29 @@ file0.close()
 
 # Extract data from test.txt and store in padded_test_data
 file1 = open(sources[1], 'r')
+
+training_data_dict = {}
 while line := file1.readline():
     tokenized_line = line.split()
+
     paddedLine = ""
     paddedLine += "<s>"
     for token in tokenized_line:
-        paddedLine += token.lower() + " "
+        word = token.lower()
+        if word not in training_data_dict:
+            training_data_dict.update({word: 1})
+        else:
+            old_count = training_data_dict.get(word)
+            training_data_dict.update({word: old_count + 1})
+        paddedLine += word + " "
+
     paddedLine += "</s>"
     padded_test_data.append(paddedLine)
 file1.close()
 
-
+# key: word ,value: frequency
+# Replace all words occurring in the training data once with the token <unk>. Every word in the test data not seen in the
+# training data should be treated as <unk>
+print("Hello")
 
 
