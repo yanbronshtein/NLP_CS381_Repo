cfe4a8411ebfcae8714fae8a5fc0c3a9a8b75bbc
Yaniv Bronshtein <yanbronshtein@gmail.com>
2020-03-14 23:43:12 -0400

    Made code ugly but working

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -4,10 +4,9 @@ import copy
 
 path = "/Users/yanivbronshtein/Coding/QueensCollege/NLP_CS381_Repo/HW1"
 
-sources = [path + "/train.txt", path + "/test.txt"]  # todo: change back to original nigga
+# sources = [path + "/train.txt", path + "/test.txt"]  # todo: change back to original nigga
 
-
-# sources = [path + "/train_small.txt", path + "/test_small.txt"]
+sources = [path + "/train_small.txt", path + "/test_small.txt"]
 
 
 # This function is used to tokenize and path data read from train.txt and test.txt
@@ -26,76 +25,50 @@ def pad_and_tokenize_file_data(file_path):
     return tokenized_padded_data
 
 
-train_data_dict = {}
-
-
-def pre_process_train_data(file_path):
-    tokenized_train_data = pad_and_tokenize_file_data(file_path)
-
-    for line in tokenized_train_data:
-        for word in line:
-            train_data_dict[word] = 1 if word not in train_data_dict else train_data_dict[word] + 1
-
-    rare_words_dict = {key: value for (key, value) in train_data_dict.items() if
-                       value == 1 and value != "<s>" and value !=
-                       "</s>"}  # rare_words_dict contains only those words that are not start or end symbols that 
-    # appear only once in training
-    # those words that appear once in training
-
-    # Replace each word in tokenized_train_data with <unk> if it only appears once in rare_words_dict 
-    for line in tokenized_train_data:
-        for i in range(0, len(line)):
-            if line[i] in rare_words_dict:
-                line[i] = "<unk>"
-
-    return tokenized_train_data
-
-
-def pre_process_test_data(file_path):
-    tokenized_test_data = pad_and_tokenize_file_data(file_path)
-
-    # Replace each word in tokenized_test_data with "<unk>" if it does not appear in testing
-    for line in tokenized_test_data:
-        for i in range(0, len(line)):
-            if line[i] not in train_data_dict:
-                line[i] = "<unk>"
-
-    return tokenized_test_data
-
-
-processed_train_data = pre_process_train_data(sources[0])
-processed_test_data = pre_process_test_data(sources[1])
-
-
-def count_total_word_tokens(data):
-    token_count = 0
+def populate_dict(data):
+    dictionary = {}
     for line in data:
-        token_count += len(line)
-    return token_count
+        for word in line:
+            dictionary[word] = 1 if word not in dictionary else dictionary[word] + 1
+    return dictionary
 
 
-# bigram_model = {}
-# total_number_of_bigrams = 0
-# tokenized_training_unk_splitted = tokenized_training_unk.split()
-# for i in range(len(tokenized_training_unk_splitted)-1):
-#     bigram_key = tokenized_training_unk_splitted[i] + "," + tokenized_training_unk_splitted[i+1]
-#     if bigram_model.get(bigram_key) == None:
-#         bigram_model[bigram_key] = 1
-#     else:
-#         bigram_model[bigram_key] = bigram_model[bigram_key] + 1
-#     total_number_of_bigrams+=1
+tokenized_train_data_before_unk = pad_and_tokenize_file_data(sources[0])
+train_data_dict_before_unk = populate_dict(tokenized_train_data_before_unk)
+rare_words_dict = {key: value for (key, value) in train_data_dict_before_unk.items() if
+                   value == 1 and value != "<s>" and value !=
+                   "</s>"}  # rare_words_dict contains only those words that are not start or end symbols that
+# appear only once in training
+# those words that appear once in training
+
+tokenized_train_data_after_unk = copy.deepcopy(tokenized_train_data_before_unk)
+for line in tokenized_train_data_after_unk:
+    for i in range(0, len(line)):
+        if line[i] in rare_words_dict:
+            line[i] = "<unk>"
+
+train_data_dict_after_unk = populate_dict(tokenized_train_data_after_unk)
+token_count = 0
+for line in tokenized_train_data_after_unk:
+    token_count += len(line)
+print("Q2: How many word tokens are there in the training corpus?\n" + str(token_count))
 
 
-unique_word_count_training = len(train_data_dict)
-print("Q1: How many word types (unique words) are there in the training corpus?\n "
-      "Please include the padding symbols and the unknown token.\n" + str(unique_word_count_training))
+tokenized_test_data_before_unk = pad_and_tokenize_file_data(sources[1])
+test_dict_before_unk = populate_dict(tokenized_test_data_before_unk)
+tokenized_test_data_after_unk = copy.deepcopy(tokenized_test_data_before_unk)
+for line in tokenized_test_data_after_unk:
+    for i in range(0, len(line)):
+        if line[i] not in train_data_dict_after_unk:
+            line[i] = "<unk>"
 
-total_word_count_training = count_total_word_tokens(processed_train_data)
-print("Q2: How many word tokens are there in the training corpus?\n" + str(total_word_count_training))
+test_dict_after_unk = populate_dict(tokenized_test_data_after_unk)
+
+
 
 
 def train_unigram_mle():
-    unigram_model = copy.deepcopy(train_data_dict)
+    unigram_model = copy.deepcopy(train_data_dict_before_unk)
 
     for word in unigram_model:
         unigram_model[word] = unigram_model[word] / total_word_count_training
@@ -104,10 +77,22 @@ def train_unigram_mle():
 
 
 unigram_model = train_unigram_mle()
-print("")
 
+print("Q3: What percentage of word tokens and word types in the test corpus did not occur in training\n"
+      " (before you mapped the unknown words to <unk> in training and test data)? \n"
+      "Please include the padding symbols in your calculations.")
+# percentage_word_tokens_in
 sum = 0
 for word in unigram_model:
     sum += unigram_model[word]
 
 print(sum)
+
+
+def train_bigram_mle():
+    bigram_model = copy.deepcopy(train_data_dict_before_unk)
+
+    for word in unigram_model:
+        unigram_model[word] = unigram_model[word] / total_word_count_training
+
+    return unigram_model
